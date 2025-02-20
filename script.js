ae63ad27780af8ef4442e7ee30221f1923843297
rangerfan22916 <526DLichtenthal@frhsd.com>
2025-02-20 10:35:44 -0500

    everything looks pretty well

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const navbarLinks = document.querySelectorAll(".nav-link");
-
-  navbarLinks.forEach((link) => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault();
-      const target = document.querySelector(this.getAttribute("href"));
-      if (target) {
-        window.scrollTo({
-          top: target.offsetTop - 70,
-          behavior: "smooth",
-        });
-      }
-    });
-  });
+  generateQuote();
 });
+
+const quotes = [
+  "Courage and perseverance have a magical talisman, before which difficulties disappear and obstacles vanish into air.",
+  "Patience and perseverance have a magical effect before which difficulties disappear and obstacles vanish.",
+  "Try and fail, but don’t fail to try.",
+  "If your actions inspire others to dream more, learn more, do more and become more, you are a leader.",
+  "The influence of each human being on others in this life is a kind of immortality.",
+];
+
+function generateQuote() {
+  document.getElementById("quote").innerText =
+    quotes[Math.floor(Math.random() * quotes.length)];
+}
